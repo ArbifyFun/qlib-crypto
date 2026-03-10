228855628a117234d8e6a59e23bfb838e591ac4a
ArbifyFun <geekArvin@gmail.com>
2026-03-10 16:20:46 +0800

    Fix incremental updater timestamp normalization

--- qlib_crypto/collector/incremental.py
+++ qlib_crypto/collector/incremental.py
@@ -43,7 +43,7 @@ class IncrementalUpdater:
     def _infer_start(self, df: pd.DataFrame) -> pd.Timestamp:
         last_dt = pd.to_datetime(df["date"]).max()
         step = pd.Timedelta(days=1) if self.interval == "1d" else pd.Timedelta(minutes=1)
-        return pd.Timestamp(last_dt) + step
+        return self._normalize_ts(pd.Timestamp(last_dt) + step)
 
     @staticmethod
     def _normalize_ts(ts: pd.Timestamp) -> pd.Timestamp:
@@ -69,7 +69,7 @@ class IncrementalUpdater:
                 continue
             qlib_symbol = str(old_df["symbol"].iloc[0])
             raw_symbol = _to_exchange_symbol(self.exchange, qlib_symbol)
-            start_ts = self._normalize_ts(self._infer_start(old_df))
+            start_ts = self._infer_start(old_df)
             if start_ts >= end_ts:
                 continue
             new_df = collector.get_data(raw_symbol, self.interval, start_ts, end_ts)
